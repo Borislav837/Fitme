29e7a76763cb3044cd7a49e07fdee3d9bdfb1774
borislav837 <serafimovborislav6@gmail.com>
2026-04-04 17:14:57 +0300

    testing

---
+++ Web.Tests/ExercisesControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Web.Controllers;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Tests;
+
+[TestFixture]
+public class ExercisesControllerTests
+{
+    private AppDbContext _context;
+    private ExercisesController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new AppDbContext(options);
+        _controller = new ExercisesController(_context);
+    }
+
+    [Test]
+    public async Task Index_ReturnsViewWithModel()
+    {
+        // Arrange
+        var cat = new Category { Id = 1, Name = "Силови" };
+        _context.Categories.Add(cat);
+        _context.Exercises.Add(new Exercise { Name = "Test Ex", CategoryId = 1 });
+        await _context.SaveChangesAsync();
+
+        // Act
+        var result = await _controller.Index();
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<ViewResult>());
+        var viewResult = result as ViewResult;
+        var model = viewResult?.Model as List<Exercise>;
+        Assert.That(model?.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task Edit_ReturnsNotFound_WhenIdIsNull()
+    {
+        // Act
+        var result = await _controller.Edit(null);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<NotFoundResult>());
+    }
+
+    [Test]
+    public async Task Create_Post_ValidModel_SavesAndRedirects()
+    {
+        // Arrange
+        var exercise = new Exercise { Name = "New Exercise", CategoryId = 1 };
+
+        // Act
+        var result = await _controller.Create(exercise);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+        Assert.That(_context.Exercises.Count(), Is.EqualTo(1));
+    }
+
+    [TearDown]
+    public void TearDown() 
+    {
+        _context.Dispose();
+        _controller.Dispose();
+    }
+}
---
+++ Web.Tests/WorkoutsControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Web.Controllers;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Tests;
+
+[TestFixture]
+public class WorkoutsControllerTests
+{
+    private AppDbContext _context;
+    private WorkoutsController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new AppDbContext(options);
+        _controller = new WorkoutsController(_context);
+    }
+
+    [Test]
+    public async Task DeleteConfirmed_RemovesWorkoutAndLogs()
+    {
+        // Arrange: Create Workout with a Log
+        var workout = new Workout { Id = 1, Name = "Test Workout", Date = DateTime.Now };
+        var log = new WorkoutLog { Id = 1, WorkoutId = 1, ExerciseId = 1 };
+        
+        _context.Workouts.Add(workout);
+        _context.WorkoutLogs.Add(log);
+        await _context.SaveChangesAsync();
+
+        // Act
+        var result = await _controller.DeleteConfirmed(1);
+
+        // Assert
+        Assert.That(_context.Workouts.Find(1), Is.Null);
+        Assert.That(_context.WorkoutLogs.Find(1), Is.Null);
+        Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+    }
+
+    [Test]
+    public async Task Details_ReturnsNotFound_WhenIdIsNull()
+    {
+        // Act
+        var result = await _controller.Details(null);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<NotFoundResult>());
+    }
+
+    [Test]
+    public async Task AddLog_SavesAndRedirects()
+    {
+        // Arrange
+        var log = new WorkoutLog { WorkoutId = 1, ExerciseId = 1, Sets = 3 };
+
+        // Act
+        var result = await _controller.AddLog(log);
+
+        // Assert
+        Assert.That(_context.WorkoutLogs.Count(), Is.EqualTo(1));
+        Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+    }
+
+    [TearDown]
+    public void TearDown() 
+    {
+        _context.Dispose();
+        _controller.Dispose();
+    }
+}
